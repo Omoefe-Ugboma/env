dbccf24d71c98a5963b86b13892c4c69b0ca84ca
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-21 13:43:14 +0100

    Update on the end of today's class

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,6 +1,15 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from employees.models import Employee
+
 
 def home(request):
-    return render(request,'home.html')
+    employees = Employee.objects.all()
+    context = {
+        'employees': employees
+    }
+    return render(request,'home.html', context)
+
+# def home(request):
+#     return render(request,'home.html')
     # return HttpResponse('Welcome')
