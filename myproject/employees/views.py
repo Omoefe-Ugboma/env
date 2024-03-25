c1a6826c9b676858ada084bbc5f7a1a4150003e0
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-03-25 10:13:50 +0100

    User details section

--- myproject/employees/views.py
+++ myproject/employees/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_list_or_404, render
 
+from employees.models import Employee
 # Create your views here.
+def employee_detail(request, pk):
+    employee = get_list_or_404(Employee, pk=pk)
+    context = {
+        'employee':employee
+    }
+    return render(request, 'employee_detail.html', context)
+ 
